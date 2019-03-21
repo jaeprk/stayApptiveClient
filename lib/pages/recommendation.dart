8d7754e0341cda11f779798e9b957d4c6a1c52a9
Minjae Park <jaeprk94@gmail.com>
2019-03-21 06:51:23 -0700

    ui hot update

--- lib/pages/recommendation.dart
+++ lib/pages/recommendation.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-import '../widgets/steps_indicator.dart';
-import '../widgets/active_minutes_indicator.dart';
 import '../scoped-models/user_model.dart';
+import '../widgets/score_indicator.dart';
+import '../widgets/recommendation_indicator.dart';
 
 class Recommendation extends StatefulWidget {
   @override
@@ -14,16 +14,13 @@ class _RecommendationState extends State<Recommendation> {
   Widget build(BuildContext context) {
     return ScopedModelDescendant<UserModel>(builder: (context, child, model) {
       return Scaffold(
-        appBar: AppBar(title: Text('StayApptive')),
+        appBar: AppBar(title: Text('Recommandation')),
         body: Column(
           children: [
-            StepsIndicator(),
-            ActiveMinutesIndicator(),
-            RaisedButton(
-                child: Text('healthState'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/healthState');
-                })
+            Container(margin: EdgeInsets.all(20)),
+            RecommendationIndicator(),
+            Container(margin: EdgeInsets.all(30)),
+            ScoreIndicator(),
           ],
         ),
         backgroundColor: Colors.black,
